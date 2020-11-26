7e140392d4bc148e3cf4e509925d8b119cb58094
BoneGames <44772942+BoneGames@users.noreply.github.com>
2020-11-27 00:32:18 +1100

    Update run.sh

--- docker/images/tmux/run.sh
+++ docker/images/tmux/run.sh
@@ -12,9 +12,9 @@ docker run \
   --device /dev/tty$DESKTOP_ENVIRONMENT_HOST_TTY \
   --env DISPLAY \
   --interactive \
-  --name $DESKTOP_ENVIRONMENT_CONTAINER_IMAGE-$IMAGE \
+  --name desktop-environment-tmux \
   --network host \
   --rm \
   --volume /var/run/docker.sock:/var/run/docker.sock \
   --volume DESKTOP_ENVIRONMENT_STATE_TMUX:$DESKTOP_ENVIRONMENT_STATE_TMUX \
-  $DESKTOP_ENVIRONMENT_REGISTRY/$DESKTOP_ENVIRONMENT_CONTAINER_IMAGE-$IMAGE:$DESKTOP_ENVIRONMENT_CONTAINER_TAG
+  desktop-environment-tmux:latest
